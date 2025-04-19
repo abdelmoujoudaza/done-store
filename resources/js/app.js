708276c7da3d2fb9e2dd0c44f572172dcc0e79f4
Abdelmoujoud <abdelmoujoudaza@gmail.com>
2025-04-19 18:21:17 +0100

    Setup Vuejs in project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+// import './bootstrap';
+
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { ZiggyVue } from 'ziggy-js';
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+});
